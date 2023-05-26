c610893af4d5696121b9654b5d2414d4a340c811
amrtarek456 <99276794+amrtarek456@users.noreply.github.com>
2023-05-26 22:29:16 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -20,7 +20,7 @@ def checkPomVersion(){
                def new_version = readMavenPom file: './pom.xml'
                println(new_version.version)
                sh 'git show HEAD^:./pom.xml > check.xml'
-               //git show HEAD^:pom.xml HEAD^ -> last commit HEAD^^ -> second last commit
+               //git show HEAD^:pom.xml HEAD -> last commit HEAD^ -> second last commit
                def old_version = readMavenPom file: 'check.xml'
                println(old_version.version)
               if(new_version.version == old_version.version)
@@ -31,34 +31,34 @@ def checkPomVersion(){
               
         }
 }
-def pushJar(){
-script{
-                def mavenPom = readMavenPom file: './pom.xml'
-                nexusArtifactUploader artifacts: [[
-                artifactId: 'numeric',
-                classifier: '',
-                file: "target/numeric-${mavenPom.version}.jar",
-                type: 'jar']],
-                credentialsId: "NEXUS_CRED",
-                groupId: 'com.devsecops',
-                nexusUrl: '40.121.81.242:8081/',
-                nexusVersion: 'nexus3', 
-                protocol: 'http',
-                repository: 'myapp',
-                version: "${mavenPom.version}"
-                }
-}
+// def pushJar(){
+// script{
+//                 def mavenPom = readMavenPom file: './pom.xml'
+//                 nexusArtifactUploader artifacts: [[
+//                 artifactId: 'numeric',
+//                 classifier: '',
+//                 file: "target/numeric-${mavenPom.version}.jar",
+//                 type: 'jar']],
+//                 credentialsId: "NEXUS_CRED",
+//                 groupId: 'com.devsecops',
+//                 nexusUrl: '40.121.81.242:8081/',
+//                 nexusVersion: 'nexus3', 
+//                 protocol: 'http',
+//                 repository: 'myapp',
+//                 version: "${mavenPom.version}"
+//                 }
+// }
 
-def pushImage(){
-          echo 'Building Image ...'
-          sh "docker build -t 40.121.81.242:8083/app:${BUILD_NUMBER} ."
-          echo 'Pushing image to docker hosted rerpository on Nexus'
-          withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'PSW', usernameVariable: 'USER')]){
-          sh "echo ${PSW} | docker login -u ${USER} --password-stdin 40.121.81.242:8083"
-          sh "docker push 40.121.81.242:8083/app:${BUILD_NUMBER}"
-          sh "docker image rm 40.121.81.242:8083/app:${BUILD_NUMBER}"
-    }
-}
+// def pushImage(){
+//           echo 'Building Image ...'
+//           sh "docker build -t 40.121.81.242:8083/app:${BUILD_NUMBER} ."
+//           echo 'Pushing image to docker hosted rerpository on Nexus'
+//           withCredentials([usernamePassword(credentialsId: 'nexus', passwordVariable: 'PSW', usernameVariable: 'USER')]){
+//           sh "echo ${PSW} | docker login -u ${USER} --password-stdin 40.121.81.242:8083"
+//           sh "docker push 40.121.81.242:8083/app:${BUILD_NUMBER}"
+//           sh "docker image rm 40.121.81.242:8083/app:${BUILD_NUMBER}"
+//     }
+// }
 
 
 return this
